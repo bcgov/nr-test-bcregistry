a8bad1cf97779f52318e3ed38399d1eb7de82138
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-07-31 10:31:55 -0700

    chore: Update BcRegistryController to use Mono for document retrieval and add visibility modifier

--- src/main/java/ca/bc/gov/bc_registry_test/controller/BcRegistryController.java
+++ src/main/java/ca/bc/gov/bc_registry_test/controller/BcRegistryController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(value = "/api/bcregistry", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -23,7 +24,7 @@ public class BcRegistryController {
 
 
   @GetMapping("/facets")
-  Flux<BcRegistryFacetSearchResultEntryDto> searchByFacets(
+  public Flux<BcRegistryFacetSearchResultEntryDto> searchByFacets(
       @RequestParam(name = "name", required = false) String name,
       @RequestParam(name = "identifier", required = false) String identifier
   ) {
@@ -31,8 +32,8 @@ public class BcRegistryController {
   }
 
   @GetMapping("/documents/{identifier}")
-  Flux<BcRegistryDocumentDto> getDocument(@PathVariable String identifier) {
-    return service.requestDocumentData(identifier);
+  public Mono<BcRegistryDocumentDto> getDocument(@PathVariable String identifier) {
+    return service.requestDocumentData(identifier).next();
   }
 
 }
